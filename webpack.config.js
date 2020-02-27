d7c303c0a0a68c8a670085179fceaf678a8190c5
Jakob Skjerning <jakob@mentalized.net>
2020-02-27 18:00:26 +0100

    Compile styles with Webpack/Dart Sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,28 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    main: './source/javascripts/site.js'
+    main: './source/javascripts/site.js',
+    styles: './source/stylesheets/site.css.scss'
   },
 
   output: {
     path: __dirname + '/.tmp/webpack-build',
     filename: 'javascripts/[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
 };
